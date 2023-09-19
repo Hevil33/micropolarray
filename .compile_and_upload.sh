545e1f4638ecfa62f65ca20e47f12ae65acaafec
herve.haudemand <herve.haudemand@inaf.it>
2023-09-19 11:58:46 +0200

    added rtd theme dependency and rew.txt compile

--- .compile_and_upload.sh
+++ .compile_and_upload.sh
@@ -2,5 +2,6 @@
 pytest ./micropolarray/test_v2.py # first test
 #pdoc --html --force --output-dir ./docs micropolarray
 sphinx-apidoc -o docs micropolarray/
+pip-compile pyproject.toml
 python3 -m build
 twine upload --verbose --skip-existing dist/*
--- docs/conf.py
+++ docs/conf.py
@@ -5,32 +5,32 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'micropolarray'
-copyright = '2023, Herve Haudemand'
-author = 'Herve Haudemand'
-release = '1.0.6'
+project = "micropolarray"
+copyright = "2023, Herve Haudemand"
+author = "Herve Haudemand"
+release = "1.0.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
