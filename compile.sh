071c144f035473ab210eb921ff4a6e2a38ee16fd
herve.haudemand <herve.haudemand@inaf.it>
2023-09-19 11:29:40 +0200

    version requirements changed to python 3.8

--- compile.sh
+++ compile.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 pytest ./micropolarray/test_v2.py # first test
+#pdoc --html --force --output-dir ./docs micropolarray
+sphinx-apidoc -o docs micropolarray/
 python3 -m build
-pdoc --html --force --output-dir ./docs micropolarray
-pandoc --metadata=title:"MyProject Documentation"               \
-           --from=markdown+abbreviations+tex_math_single_backslash  \
-           --pdf-engine=xelatex --variable=mainfont:"DejaVu Sans"   \
-           --toc --toc-depth=4 --output=pdf.pdf  pdf.md
+twine upload --verbose --skip-existing dist/*
