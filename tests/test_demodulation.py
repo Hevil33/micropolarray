23cb07cc5f5324538daf6e189f8a7dca13d9c0c5
herve.haudemand <herve.haudemand@inaf.it>
2023-09-21 16:47:39 +0200

    test files refactoring

--- tests/test_demodulation.py
+++ tests/test_demodulation.py
@@ -7,156 +7,11 @@ import numpy as np
 import pytest
 from astropy.io import fits
 from scipy.optimize import curve_fit
+from test_utils import dummy_data, generate_polarized_data
 
 import micropolarray as ml
 from micropolarray.polarization_functions import AoLP, DoLP, pB
 from micropolarray.processing.demodulation import Malus
-
-
-@pytest.fixture(autouse=True)
-def dummy_data():
-    """Dummy data factory"""
-
-    def _make_dummy_data(dimension):
-        dummydata = np.zeros(shape=(dimension, dimension))
-        dummydata[0::2, 0::2] = 1
-        dummydata[0::2, 1::2] = 2
-        dummydata[1::2, 0::2] = 3
-        dummydata[1::2, 1::2] = 4
-        return dummydata
-
-    return _make_dummy_data
-
-
-def generate_polarized_image(
-    shape, S, angle_rad=0, t=1, eff=1, angles_list=[0, 45, -45, 90]
-):
-    ones = np.ones(shape=shape)
-    angles = np.array([np.deg2rad(angle) for angle in angles_list])
-    subimages = np.array(
-        [ones * S * Malus(angle_rad, t, eff, angle) for angle in angles]
-    )
-    return ml.MicropolImage(ml.merge_polarizations(subimages))
-
-
-class TestMicropolImage:
-    def write_temp_image(self, tmp_path, data):
-        """Writes images to the temp folder for testing"""
-        image = fits.PrimaryHDU(
-            data=data, do_not_scale_image_data=True, uint=False
-        )
-        image.writeto(tmp_path / "sample_image.fits")
-
-    def test_image_initialization(self, dummy_data, tmp_path):
-        """Tests the initialization of both Image and MicroPolArrayImage"""
-        dummy_data_16 = dummy_data(16)
-        self.write_temp_image(tmp_path, dummy_data_16)
-        for ImageClass in [ml.Image, ml.MicropolImage]:
-            image = ImageClass(dummy_data_16)
-            assert np.all(image.data == dummy_data_16)
-
-            image = ImageClass(str(tmp_path / "sample_image.fits"))
-            assert np.all(image.data == dummy_data_16)
-
-            image = ImageClass(image)
-            assert np.all(image.data == dummy_data_16)
-
-    def test_image_writing(self, dummy_data, tmp_path):
-        """Tests the saving of both Image and MicroPolArrayImage"""
-        dummy_data_16 = dummy_data(16)
-        for image_type in [ml.Image, ml.MicropolImage]:
-            image = image_type(dummy_data_16)
-            image.save_as_fits(str(tmp_path / "image.fits"))
-
-    def test_dark_and_flat_correction(self, dummy_data, tmp_path):
-        # test dark
-        dummy_data_16 = dummy_data(16)
-        dark_data = dummy_data(16)
-        dark_image = ml.MicropolImage(dark_data)
-        dummy_image = ml.MicropolImage(dummy_data_16, dark=dark_image)
-        assert np.all(dummy_image.data == 0.0)
-        assert np.all(dummy_image.DoLP.data == 0.0)
-        # test flat
-        signal = 4.0
-        dummy_data_16 = np.ones(shape=(16, 16)) * signal
-        flat_image = ml.MicropolImage(dummy_data_16 * np.random.random(1))
-        dummy_image = ml.MicropolImage(dummy_data_16, flat=flat_image)
-        assert np.all(dummy_image.data == signal)
-
-    def test_demosaic(self, dummy_data, tmp_path):
-        """Tests demosaic operation and demosaic writing"""
-        dummy_data_16 = dummy_data(16)
-        # test mean
-        image = ml.MicropolImage(dummy_data_16, demosaic_mode="mean")
-        image = image.demosaic()
-        for idx, demo_image in enumerate(image.demosaiced_images):
-            assert np.all(demo_image == np.full((16, 16), (idx + 1) / 4.0))
-
-        # test adjacent
-        image = ml.MicropolImage(dummy_data_16, demosaic_mode="adjacent")
-        image = image.demosaic()
-        for idx, demo_image in enumerate(image.demosaiced_images):
-            assert np.all(demo_image == np.full((16, 16), (idx + 1)))
-
-        # test writing
-        image.save_demosaiced_images_as_fits(
-            str(tmp_path / "demosaiced_images.fits")
-        )
-
-    def test_rebinning(self, dummy_data):
-        """Tests 2x2 and 4x4 binning (the other will be supposedly fine)"""
-        dummy_data_16 = dummy_data(16)
-
-        binned_image_2 = ml.MicropolImage(dummy_data_16).rebin(2)
-        assert np.all(binned_image_2.data == dummy_data(8) * 4)
-
-        binned_image_4 = ml.MicropolImage(dummy_data_16).rebin(4)
-        assert np.all(binned_image_4.data == dummy_data(4) * 16)
-
-    def test_pol_parameters(self, dummy_data):
-        """Test if polarization parameters are correcly computed"""
-
-        def test_theo_stokes(image, I, Q, U):
-            assert np.all(image.I.data == I)
-            assert np.all(image.Q.data == Q)
-            assert np.all(image.U.data == U)
-
-            assert np.all(
-                image.AoLP.data == 0.5 * np.arctan2(U, Q) * half_ones
-            )
-            assert np.all(image.pB.data == np.sqrt(Q * Q + U * U) * half_ones)
-            assert np.all(
-                image.DoLP.data == np.sqrt(Q * Q + U * U) * half_ones / I
-            )
-
-        array_side = 16
-        dummy_data_16 = dummy_data(array_side)
-        half_ones = np.ones(shape=(int(array_side / 2), int(array_side / 2)))
-
-        image = ml.MicropolImage(dummy_data_16)
-
-        for i in range(4):
-            assert np.all(image.single_pol_subimages[i] == i + 1)
-
-        angles = [0, 45, -45, 90]
-        numbers = [1.0, 2.0, 3.0, 4.0]
-        for angle, n in zip(angles, numbers):
-            assert np.all(
-                image.single_pol_subimages[image.angle_dic[angle]] == n
-            )
-
-        I = 0.5 * (1 + 2 + 3 + 4)
-        Q = 1.0 - 4.0
-        U = 2.0 - 3.0
-        test_theo_stokes(image, I, Q, U)
-
-        new_angle_dic = {45: 0, 0: 1, 90: 2, -45: 3}
-        image = ml.MicropolImage(dummy_data_16, angle_dic=new_angle_dic)
-        assert image.angle_dic == new_angle_dic
-
-        Q = 2.0 - 3.0
-        U = 1.0 - 4.0
-        test_theo_stokes(image, I, Q, U)
 
 
 class TestDemodulation:
@@ -209,8 +64,14 @@ class TestDemodulation:
         shape = (side, side)
         ones = np.ones(shape=shape)
         for pol, pol_rad in zip(polarizations, pols_rad):
-            result_image = generate_polarized_image(
-                shape=shape, S=input_signal, angle_rad=pol_rad, t=t, eff=eff
+            result_image = ml.MicropolImage(
+                generate_polarized_data(
+                    shape=shape,
+                    S=input_signal,
+                    angle_rad=pol_rad,
+                    t=t,
+                    eff=eff,
+                )
             )
             result_image.save_as_fits(tmp_path / f"pol_{int(pol)}.fits")
         if False:  # check that fit will be ok
@@ -243,8 +104,10 @@ class TestDemodulation:
         )
 
         # image polarized with phi=0 uniform, t=1, eff=1
-        ideal_image = generate_polarized_image(
-            shape=shape, S=input_signal, angle_rad=0, t=1, eff=1
+        ideal_image = ml.MicropolImage(
+            generate_polarized_data(
+                shape=shape, S=input_signal, angle_rad=0, t=1, eff=1
+            )
         )
         assert np.all(ideal_image.I.data == input_signal)
         assert np.all(ideal_image.Q.data == input_signal)
@@ -255,8 +118,10 @@ class TestDemodulation:
 
         demodulator = ml.Demodulator(output_str)
         test_angle = np.deg2rad(30)
-        example_image = generate_polarized_image(
-            shape, S=input_signal, angle_rad=test_angle, t=t, eff=eff
+        example_image = ml.MicropolImage(
+            generate_polarized_data(
+                shape, S=input_signal, angle_rad=test_angle, t=t, eff=eff
+            )
         )
         example_image = example_image.demodulate(demodulator)
         if False:
@@ -299,8 +164,10 @@ class TestDemodulation:
             return angle
 
         for dummy_angle in np.arange(0, np.pi, 0.1):
-            polarized_image = generate_polarized_image(
-                shape, input_signal, dummy_angle, t, eff
+            polarized_image = ml.MicropolImage(
+                generate_polarized_data(
+                    shape, input_signal, dummy_angle, t, eff
+                )
             )
             simple = np.round(np.mean(polarized_image.AoLP.data), 1)
             simples.append(simple)
